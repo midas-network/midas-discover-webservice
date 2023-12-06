cbc8989c82d3982b6681509d0cc129cd33a472c7
Braun,Naomi <nbraun@ctsi.ufl.edu>
2023-12-06 15:04:19 -0500

    Add routes for getting values per category

--- project/queries/routes.py
+++ project/queries/routes.py
@@ -17,6 +17,94 @@ def connect_to_db():
 
     return conn
 
+
+@midas_blueprint.route('/papers/', methods=['GET'])
+def get_full_paper_list(is_internal=False):
+    conn = connect_to_db()
+    cur = conn.cursor()
+
+    q = 'SELECT DISTINCT paperid FROM pdetails'
+    cur.execute(q)
+    rows = cur.fetchall()
+    papers = {'papers': [x['paperid'] for x in rows]}
+    if is_internal:
+        return papers
+    else:
+        return make_response(jsonify(papers), 200)
+
+
+@midas_blueprint.route('/orgs/', methods=['GET'])
+def get_full_org_list(is_internal=False):
+    conn = connect_to_db()
+    cur = conn.cursor()
+
+    q = 'SELECT DISTINCT orgid FROM p2org'
+    cur.execute(q)
+    rows = cur.fetchall()
+    orgs = {'orgs': [x['orgid'] for x in rows]}
+    if is_internal:
+        return orgs
+    else:
+        return make_response(jsonify(orgs), 200)
+
+
+@midas_blueprint.route('/authors/', methods=['GET'])
+def get_full_author_list(is_internal=False):
+    conn = connect_to_db()
+    cur = conn.cursor()
+
+    q = 'SELECT DISTINCT authorid FROM p2au'
+    cur.execute(q)
+    rows = cur.fetchall()
+    authors = {'authors': [x['authorid'] for x in rows]}
+    if is_internal:
+        return authors
+    else:
+        return make_response(jsonify(authors), 200)
+
+
+@midas_blueprint.route('/grants/', methods=['GET'])
+def get_full_grant_list(is_internal=False):
+    conn = connect_to_db()
+    cur = conn.cursor()
+
+    q = 'SELECT DISTINCT grantid FROM g2p'
+    cur.execute(q)
+    rows = cur.fetchall()
+    grants = {'grants': [x['grantid'] for x in rows]}
+    if is_internal:
+        return grants
+    else:
+        return make_response(jsonify(grants), 200)
+
+
+@midas_blueprint.route('/terms/', methods=['GET'])
+def get_full_term_list(is_internal=False):
+    conn = connect_to_db()
+    cur = conn.cursor()
+
+    q = 'SELECT DISTINCT term FROM pcount'
+    cur.execute(q)
+    rows = cur.fetchall()
+    terms = {'terms': [x['term'] for x in rows]}
+    if is_internal:
+        return terms
+    else:
+        return make_response(jsonify(terms), 200)
+
+
+@midas_blueprint.rout('/getSearchData/', methods=['GET'])
+def get_search_data():
+    papers = get_full_paper_list(True)
+    orgs = get_full_org_list(True)
+    authors = get_full_author_list(True)
+    grants = get_full_grant_list(True)
+    terms = get_full_term_list(True)
+
+    response = papers.update(orgs).update(authors).update(grants).update(terms)
+    return make_response(jsonify(response), 200)
+
+
 @midas_blueprint.route('/papers/overlap/', methods=['GET'])
 def get_paper_list():
     conn = connect_to_db()

a38e460c15bf4305434bd1e0535c5aad4bc9437b
John Levander <john.d.levander@gmail.com>
2024-01-21 17:25:11 -0500

    trying another way to request authors in the organization endpoint

--- project/queries/routes.py
+++ project/queries/routes.py
@@ -451,9 +451,8 @@ def get_org_list():
     formatted_ids = []
     if keys[withPeople]:
         #change this to authorid "in"?
-        q += 'SELECT DISTINCT orgid FROM adetails WHERE authorid in (%s)'
-        in_authors = ', '.join(map(lambda x: '%s', request.json[PEOPLE]))
-        q = q % in_authors
+        authors = ','.join(['%s'] * len(request.json[PEOPLE]))
+        q += 'SELECT DISTINCT orgid FROM adetails WHERE authorid IN (%s)' % authors, request.json[PEOPLE]
         #formatted_ids.append(request.json[PEOPLE])
     if keys[withKeywords]:
         for term in request.json[KEYWORDS]:
